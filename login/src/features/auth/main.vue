a629ab85406b48ed3239085b586e4d37834c5bdc
@douglasabnovato <douglasabnovato@gmail.com>
2020-06-04 18:33:51 -0300

    rodou com 34 errors

--- login/src/features/auth/main.vue
+++ login/src/features/auth/main.vue
@@ -13,55 +13,9 @@
         <!-- formulario Login -->
         <div class="login-form">
 
-          <!-- formulario sign-in -->
-          <div class="sign-in-htm">
-            <div class="group">
-              <label for="sign-in-user" class="label">Username</label>
-              <input id="sign-in-user" type="text" class="input">
-            </div>
-            <div class="group">
-              <label for="sign-in-pass" class="label">Password</label>
-              <input id="sign-in-pass" type="password" class="input" data-type="password">
-            </div>
-            <div class="group">
-              <input id="check" type="checkbox" class="check" checked>
-              <label for="check"><span class="icon"></span> Keep me Signed in</label>
-            </div>
-            <div class="group">
-              <input type="submit" class="button" value="Sign In">
-            </div>
-            <div class="hr"></div>
-            <div class="foot-lnk">
-              <a href="#forgot">Forgot Password?</a>
-            </div>
-          </div><!-- formulario sign-in - fim -->
 
-          <!-- formulario sign-up -->
-          <div class="sign-up-htm">
-            <div class="group">
-              <label for="sign-up-user" class="label">Username</label>
-              <input id="sign-up-user" type="text" class="input">
-            </div>
-            <div class="group">
-              <label for="sign-up-pass1" class="label">Password</label>
-              <input id="sign-up-pass1" type="password" class="input" data-type="password">
-            </div>
-            <div class="group">
-              <label for="sign-up-pass2" class="label">Repeat Password</label>
-              <input id="sign-up-pass2" type="password" class="input" data-type="password">
-            </div>
-            <div class="group">
-              <label for="pass" class="label">Email Address</label>
-              <input id="pass" type="text" class="input">
-            </div>
-            <div class="group">
-              <input type="submit" class="button" value="Sign Up">
-            </div>
-            <div class="hr"></div>
-            <div class="foot-lnk">
-              <label for="tab-1">Already Member?</a>
-            </div>
-          </div><!-- formulario sign-up - fim -->
+
+
 
 		    </div><!-- formulario Login - fim -->
 
