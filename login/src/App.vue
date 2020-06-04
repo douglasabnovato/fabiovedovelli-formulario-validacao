401976adf07edf03def7de690ad9f5f67612eb04
@douglasabnovato <douglasabnovato@gmail.com>
2020-06-04 16:07:35 -0300

    componentização de elementos

--- login/src/App.vue
+++ login/src/App.vue
@@ -1,47 +1,18 @@
-<template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-    <h2>@douglasabnovato</h2>
-
-  </div>
-</template>
-
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Douglas A B Novato'
-    }
-  }
-}
+
+ import VedAuth from './features/auth/main'
+
+ export default {
+   components: {
+
+     VedAuth
+
+   }
+ }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<template>
 
-h1, h2 {
-  font-weight: normal;
-}
+  <ved-auth/>
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+</template>
--- login/src/features/auth/sign-out.vue
+++ login/src/features/auth/sign-out.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-
+    <h2>@douglasabnovato</h2>
   </div>
 </template>
+
+<script>
+export default {
+}
+</script>
 
 <style>
 
